aa229f45fc81c5836a735a9379353ba678f38154
Ani <ani.zakaryan.1993@inbox.ru>
2019-12-18 00:01:24 +0400

    homework

--- index.js
+++ index.js
@@ -1,55 +1,90 @@
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
+// const http = require('http');
+// const url = require('url');
+// const fs = require('fs');
 
-const sendData   = (fileName, res) => {
-    fs.readFile(`${fileName}.html`, "utf-8", (err, data) =>{
-        try{
+// const sendData = (fileName,res) =>{
+//    fs.readFile(`${fileName}.html`,'utf-8',(err,data) =>{
+//       try{
+//          if(err.code === 'ENOENT' || err.code === 'EEXIST' || err.code === 'EMFILE' || err.code === 'EISDIR' || err.code === 'ENOTDIR'){
+//             trow (err)
+         
+//          };
+     
+//       }catch (err){
+//          console.log(err.name),
+//          console.log(err.message)
+//       }
+      
+//    });
+  
+//    res.end()
+// };
 
-        if(err){
-            
-            throw err;
-        }
-        
-    } catch(err){
-        console.log(err);
-        
-    }
-    res.end();
-    })
-   
-}
+// http.createServer((req,res) =>{
+//    res.writeHead(200);
 
-// const sendData   = (fileName, res) => {
-//     fs.readFile(`${fileName}.html`, "utf-8", (err, data) =>{
-//         try{
+//    const {query:{page}} = url.parse(req.url,true);
 
-//         if(err.code === "ENOENT" || err.code === "EMFILE" || err.code === "EEXIST" || err.code === "EACCES" || err.code === "ENODIRmp"){
-            
-//             throw err;
-//         }
-//         res.end();
-//     } catch(err){
-//         console.log(err.name);
-        
-//     }
-//     })
+//    switch(page){
+//       case 'home':sendData('index',res);break;
+//       case 'contact':sendData('contact',res);break;
+//       case 'news':sendData('news',res);break;
+//       default:sendData('404',res);break;
+//    }
+// }).listen(3000);
+
+
+// function test (a){
+//    let x = 0 ;
+//    for(let i = 0; i<a; i++){
+//      x = i;
+//    }
+//    return x;
 // }
 
-http.createServer((req,res)=>{
-   res.writeHead(200);
-   const {query: {page} } = url.parse(req.url, true);
+// console.time('func');
+// test (5000);
+// console.timeEnd('func');
 
-    switch (page) {
-        case "home" : sendData("index", res); break;
-        case "contact" : sendData("contac", res); break;
-        case "news" : sendData("news", res); break;
-        default: sendData("404", res); break;
+// console.log(process.version);
+// console.log(process.platform);
+// console.log(process.title);
+// console.log(process.moduleLoadList);
 
-    }    
-}).listen(3000)    
+// const fs = require('fs');
+// const args = process.argv.slice(2);
 
+// if(args[0] === '--file' && args[1]){
+//    fs.readFile('./'+args[1] + '.txt','utf-8',(err,data) =>{
+//       console.log(data);
+//    });
+// }
 
+// if(args[0] === '--css' && args[1]){
+//    fs.readFile('./'+args[1] + '.css','utf-8',(err,data) =>{
+//       console.log(data);
+//    });
+// 
 
+const fs = require('fs');
+const arg = require('minimist')(process.argv.slice(2));
 
+function getFileContent(fileName){
+   fs.readFile(`./${fileName}`,'utf-8',(err,data) =>{
+      if(err){
+         console.log(`./${fileName}.txt not found`);
+      }else{
+         console.log(data)
+      }
+   });
+}
 
+if(arg.f || arg.file){
+   const fileName = arg.f || arg.file;
+   getFileContent(`${fileName}.txt`)
+}  
+
+if(arg.c || arg.css){
+   const fileName = arg.c || arg.css;
+   getFileContent(`${fileName}.css`)
+} 
